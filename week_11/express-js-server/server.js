e460733dafbd2ce46d597f14970a8f7e496b398b
indefinit <kevin.siwoff@gmail.com>
2015-11-12 21:10:28 -0500

    adds server to node p5

--- week_11/express-js-server/server.js
+++ week_11/express-js-server/server.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var app = express();//need to use round brackets to activate express
 
+var PORT = process.argv[2] || 9000;
+
 //we use app.get to retrieve our request and pass the success into a 
 //callback
+//home page
 app.get('/', function(request, response){
 	response.send('successful response!');
 });
 
-var server = app.listen(4000, function(){
+//about page
+app.get('/about', function(request, response){
+  response.send('you landed on' + request.url);
+});
+
+app.get('/contact', function(request, response){
+  response.send('you landed on' + request.url);
+});
+
+
+var server = app.listen(PORT, function(){
 	var host = server.address().address;
 	var port = server.address().port;
 });
